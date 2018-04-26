0021bd885b14793ef27ae7c4b36620c4463706b8
unknown <0.perera.hishi@gmail.com>
2018-04-26 21:22:11 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<?php
+session_start();
+include 'csrf.class.php';
+ 
+$csrf = new csrf();
+ 
+ 
+// Generate Token Id and Valid
+$token_id = $csrf->get_token_id();
+$token_value = $csrf->get_token($token_id);
+
+ 
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CSRF demo</title>
+    </head>
+    <body>
+        
+        <form action="submit.php" method="post">
+        <input type="hidden" name="<?= $token_id; ?>" value="<?= $token_value; ?>" />
+        Username : <input type="text" name="<?= $form_names['user']; ?>" value="User123" /><br/><br/>
+        Password : <input type="password" name="<?= $form_names['password']; ?>" value="1234"/><br/><br/>
+        <input type="submit" value="Login"/>
+        </form>
+    </body>
+</html>
---
+++ submit.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include 'csrf.class.php';
+ 
+$csrf = new csrf();
+$token_id = $csrf->get_token_id();
+$token_value = $csrf->get_token($token_id);
+
+if($csrf->check_valid('post')):
+   var_dump($_POST[$token_id]);
+else:
+ echo 'Not Valid';
+endif;
+?>
